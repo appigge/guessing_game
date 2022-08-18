bcbeb3e0c053691e8c5e3e144fd70877971eb403
Charlotte <xcryptox@protonmail.com>
2022-08-18 12:00:42 +0100

    adding new section to generate a random number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::io;  //bring in ability to accept user input
+use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
-    let mut guess = String::new(); //mutable variable guess is bound to a new emply string
+    let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
